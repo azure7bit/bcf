3326d081720f345fa3ca1e6471bcec4614239733
Asep Saepudin <asevenfold7bit@gmail.com>
2014-10-15 02:07:03 +0700

    change domain

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -88,7 +88,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     :address => "smtp.gmail.com",
     :port => 587,
-    :domain => "bandungcoffeefestival.herokuapp.com",
+    :domain => "gmail.com",
     :user_name => "bandungcoffee.fest@gmail.com",
     :password => "youngtryishere",
     :authentication => "plain",
